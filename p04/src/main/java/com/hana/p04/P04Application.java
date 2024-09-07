17771d1048e9bb98ae78da23d60360e43c8e3a7b
mutjin08 <mutjin08@gmail.com>
2024-09-07 12:14:22 +0900

    initialized structure

---
+++ p04/src/main/java/com/hana/p04/P04Application.java
@@ -0,0 +1,13 @@
+package com.hana.p04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P04Application.class, args);
+    }
+
+}
---
+++ p04/src/test/java/com/hana/p04/P04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.p04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
